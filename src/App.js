eea90cd7583190d29c25e2a60d53d14025f810b4
Aminat Ajagbe <ajagbeaminat95@gmail.com>
2021-12-02 20:31:44 +0000

    Add app container and content

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container';
+import Card from 'react-bootstrap/Card';
 
 const App = () => {
   return (
-    <p>Hello <span>Trying google fonts</span> </p>
+    <Container fluid id="App-Container">
+      <h1>A3AJAGBE Calculator App</h1>
+	  	<Card id="Card-Main">
+		  	<Card.Header>Calc Display</Card.Header>
+			<Card.Body>This is some text within a card body.</Card.Body>
+			<Card.Footer className="lead fs-6 text-center">Designed & Developed by Yours truly</Card.Footer>
+		</Card>
+    </Container>
   );
 }
 
