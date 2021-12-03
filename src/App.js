1a38e6c624bfc0a4deff398b5030af148872c126
Aminat Ajagbe <ajagbeaminat95@gmail.com>
2021-12-03 20:50:43 +0000

    Add calc grid

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Container from 'react-bootstrap/Container';
 import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
 
 const App = () => {
   return (
@@ -9,8 +10,29 @@ const App = () => {
       <h1>A3AJAGBE Calculator App</h1>
 	  	<Card id="Card-Main">
 		  	<Card.Header>Calc Display</Card.Header>
-			<Card.Body>This is some text within a card body.</Card.Body>
-			<Card.Footer className="lead fs-6 text-center">Designed & Developed by Yours truly</Card.Footer>
+			<Card.Body>
+				<Card.Title className="bg-dark p-4"> Wow </Card.Title>
+			</Card.Body>
+			<Card.Footer className="Calc-Grid">
+				<Button size="lg" className="Calc-Btn">1</Button>
+				<Button size="lg" className="Calc-Btn">1</Button>
+				<Button size="lg" className="Calc-Btn">1</Button>
+				<Button size="lg" className="Calc-Btn">1</Button>
+				<Button size="lg" className="Calc-Btn">1</Button>
+				<Button size="lg" className="Calc-Btn">1</Button>
+				<Button size="lg" className="Calc-Btn">1</Button>
+				<Button size="lg" className="Calc-Btn">1</Button>
+				<Button size="lg" className="Calc-Btn">1</Button>
+				<Button size="lg" className="Calc-Btn">1</Button>
+				<Button size="lg" className="Calc-Btn">1</Button>
+				<Button size="lg" className="Calc-Btn">1</Button>
+				<Button size="lg" className="Calc-Btn">1</Button>
+				<Button size="lg" className="Calc-Btn">1</Button>
+				<Button size="lg" className="Calc-Btn">1</Button>
+				<Button size="lg" className="Calc-Btn">1</Button>
+				<Button size="lg" className="Calc-Btn">1</Button>
+
+			</Card.Footer>
 		</Card>
     </Container>
   );
