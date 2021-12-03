8357f87cb082051e18f85841b934c15004a55d76
Aminat Ajagbe <ajagbeaminat95@gmail.com>
2021-12-03 21:45:42 +0000

    Update commit

--- src/App.js
+++ src/App.js
@@ -9,30 +9,28 @@ const App = () => {
     <Container fluid id="App-Container">
       <h1>A3AJAGBE Calculator App</h1>
 	  	<Card id="Card-Main">
-		  	<Card.Header>A3</Card.Header>
+		  	<Card.Header>/.\</Card.Header>
 			<Card.Body>
-				<Card.Title className="bg-dark p-4" id="display"> Wow </Card.Title>
+				<Card.Title className="bg-dark p-4 text-end" id="display"> 0 </Card.Title>
 			</Card.Body>
 			<Card.Footer className="Calc-Grid p-4">
-				<Button size="lg" className="Calc-Btn" id="clear">AC</Button>
-				<Button size="lg" className="Calc-Btn" id="seven">7</Button>
-				<Button size="lg" className="Calc-Btn" id="eight">8</Button>
-				<Button size="lg" className="Calc-Btn" id="nine">9</Button>
+				<Button size="lg" className="" id="clear">AC</Button>
+				<Button size="lg" className="theme-button" id="seven">7</Button>
+				<Button size="lg" className="theme-button" id="eight">8</Button>
+				<Button size="lg" className="theme-button" id="nine">9</Button>
 				<Button size="lg" className="Calc-Btn" id="divide">/</Button>
-				<Button size="lg" className="Calc-Btn" id="four">4</Button>
-				<Button size="lg" className="Calc-Btn" id="five">5</Button>
-				<Button size="lg" className="Calc-Btn" id="six">6</Button>
+				<Button size="lg" className="theme-button" id="four">4</Button>
+				<Button size="lg" className="theme-button" id="five">5</Button>
+				<Button size="lg" className="theme-button" id="six">6</Button>
 				<Button size="lg" className="Calc-Btn" id="multiply">*</Button>
-				<Button size="lg" className="Calc-Btn" id="one">1</Button>
-				<Button size="lg" className="Calc-Btn" id="two">2</Button>
-				<Button size="lg" className="Calc-Btn" id="three">3</Button>
+				<Button size="lg" className="theme-button" id="one">1</Button>
+				<Button size="lg" className="theme-button" id="two">2</Button>
+				<Button size="lg" className="theme-button" id="three">3</Button>
 				<Button size="lg" className="Calc-Btn" id="subtract">-</Button>
 				<Button size="lg" className="Calc-Btn" id="decimal">.</Button>
-				<Button size="lg" className="Calc-Btn" id="zero">0</Button>		
+				<Button size="lg" className="theme-button" id="zero">0</Button>		
 				<Button size="lg" className="Calc-Btn" id="equals">=</Button>
 				<Button size="lg" className="Calc-Btn" id="add">+</Button>
-				
-
 			</Card.Footer>
 		</Card>
     </Container>
