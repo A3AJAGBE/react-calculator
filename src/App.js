3ba01949d1ab9077e4928966af71e60222efffe3
Aminat Ajagbe <ajagbeaminat95@gmail.com>
2021-12-03 21:08:42 +0000

    Add distinct ids to calc grid btns

--- src/App.js
+++ src/App.js
@@ -9,28 +9,29 @@ const App = () => {
     <Container fluid id="App-Container">
       <h1>A3AJAGBE Calculator App</h1>
 	  	<Card id="Card-Main">
-		  	<Card.Header>Calc Display</Card.Header>
+		  	<Card.Header>A3</Card.Header>
 			<Card.Body>
-				<Card.Title className="bg-dark p-4"> Wow </Card.Title>
+				<Card.Title className="bg-dark p-4" id="display"> Wow </Card.Title>
 			</Card.Body>
-			<Card.Footer className="Calc-Grid">
-				<Button size="lg" className="Calc-Btn">1</Button>
-				<Button size="lg" className="Calc-Btn">1</Button>
-				<Button size="lg" className="Calc-Btn">1</Button>
-				<Button size="lg" className="Calc-Btn">1</Button>
-				<Button size="lg" className="Calc-Btn">1</Button>
-				<Button size="lg" className="Calc-Btn">1</Button>
-				<Button size="lg" className="Calc-Btn">1</Button>
-				<Button size="lg" className="Calc-Btn">1</Button>
-				<Button size="lg" className="Calc-Btn">1</Button>
-				<Button size="lg" className="Calc-Btn">1</Button>
-				<Button size="lg" className="Calc-Btn">1</Button>
-				<Button size="lg" className="Calc-Btn">1</Button>
-				<Button size="lg" className="Calc-Btn">1</Button>
-				<Button size="lg" className="Calc-Btn">1</Button>
-				<Button size="lg" className="Calc-Btn">1</Button>
-				<Button size="lg" className="Calc-Btn">1</Button>
-				<Button size="lg" className="Calc-Btn">1</Button>
+			<Card.Footer className="Calc-Grid p-4">
+				<Button size="lg" className="Calc-Btn" id="clear">AC</Button>
+				<Button size="lg" className="Calc-Btn" id="seven">7</Button>
+				<Button size="lg" className="Calc-Btn" id="eight">8</Button>
+				<Button size="lg" className="Calc-Btn" id="nine">9</Button>
+				<Button size="lg" className="Calc-Btn" id="divide">/</Button>
+				<Button size="lg" className="Calc-Btn" id="four">4</Button>
+				<Button size="lg" className="Calc-Btn" id="five">5</Button>
+				<Button size="lg" className="Calc-Btn" id="six">6</Button>
+				<Button size="lg" className="Calc-Btn" id="multiply">*</Button>
+				<Button size="lg" className="Calc-Btn" id="one">1</Button>
+				<Button size="lg" className="Calc-Btn" id="two">2</Button>
+				<Button size="lg" className="Calc-Btn" id="three">3</Button>
+				<Button size="lg" className="Calc-Btn" id="subtract">-</Button>
+				<Button size="lg" className="Calc-Btn" id="decimal">.</Button>
+				<Button size="lg" className="Calc-Btn" id="zero">0</Button>		
+				<Button size="lg" className="Calc-Btn" id="equals">=</Button>
+				<Button size="lg" className="Calc-Btn" id="add">+</Button>
+				
 
 			</Card.Footer>
 		</Card>
